fd3154aff30214dbc864e70e03121f245f780f1d
Sumit Kumar Ratna <sumitkumarratna@gmail.com>
2026-02-10 01:33:59 +0530

    Final: Direct Login Mode Verified State

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ app.use('/api/documents', documentRoutes);
 app.use('/api/doctor', doctorRoutes);
 
 app.get('/', (req, res) => {
-    res.send('HealthNexus API is Running (DEPLOYED: FIXED OTP MODE)');
+    res.send('HealthNexus API is Running (DEPLOYED: DIRECT LOGIN MODE)');
 });
 
 // Start Server with Firestore Check
